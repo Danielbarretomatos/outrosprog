1989cdac7488c2f766abbb04a35eb7ec1efda182
Danielbarretomatos <54272248+Danielbarretomatos@users.noreply.github.com>
2019-08-28 10:30:41 -0300

    Add files via upload

---
+++ C prog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  
+float g1,g2,g3;
+
+g1 = 780000 * 0.46;
+g2 = 780000 * 0.32;
+g3 = 780000 * 0.22;
+
+printf("De um total de R$780.000,00\n");
+printf("O primeiro ganhador recebeu: R$%.2f \n", g1);
+printf("O segundo ganhador recebeu: R$%.2f \n", g2);
+printf("O terceiro ganhador recebeu: R$%.2f \n", g3);
+
+
+
+}
